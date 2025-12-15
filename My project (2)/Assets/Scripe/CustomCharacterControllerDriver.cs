6b448b1296812840e3279f4b94569a5e1384103b
xinxiangeaishuati <3600533874@qq.com>
2025-12-15 11:20:07 +0800

    更新人物移动和人物转向以及人物碰撞体，实现了人物上下楼梯，增加了人物抓取和人物瞬移功能

---
+++ My project (2)/Assets/Scripe/CustomCharacterControllerDriver.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CustomCharacterControllerDriver : CharacterControllerDriver
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCharacterController();
+    }
+}
